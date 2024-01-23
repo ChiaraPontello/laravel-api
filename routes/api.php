5b6ba9eb153a5cae9e0e9a942e9effad97ad2618
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-23 15:10:11 +0100

    fix api

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/projects',[ProjectController::class,'index']);
+Route::get('projects',[ProjectController::class,'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
